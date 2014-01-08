2b93556a14dc6091d1339e494a8048f461152136
Deepak D <deepakd.pc@gmail.com>
2014-01-08 18:28:09 +0530

    Modifications for youtube new html changes

--- IOSYoutubePlayer/LBYouTubeExtractor.h
+++ IOSYoutubePlayer/LBYouTubeExtractor.h
@@ -13,6 +13,8 @@ extern NSInteger const LBYouTubePlayerExtractorErrorCodeInvalidHTML;
 extern NSInteger const LBYouTubePlayerExtractorErrorCodeNoStreamURL;
 extern NSInteger const LBYouTubePlayerExtractorErrorCodeNoJSONData;
 
+typedef void (^LBYouTubeExtractorCompletionBlock)(NSURL *videoURL, NSError *error);
+
 typedef enum {
     LBYouTubeVideoQualitySmall    = 0,
     LBYouTubeVideoQualityMedium   = 1,
@@ -21,23 +23,22 @@ typedef enum {
 
 @protocol LBYouTubeExtractorDelegate;
 
-@interface LBYouTubeExtractor : NSObject {
-    LBYouTubeVideoQuality quality;
-    NSURL* youTubeURL;
-    NSURL* extractedURL;
-    id <LBYouTubeExtractorDelegate> __unsafe_unretained delegate;
-}
+@interface LBYouTubeExtractor : NSObject
 
 @property (nonatomic, readonly) LBYouTubeVideoQuality quality;
 @property (nonatomic, strong, readonly) NSURL* youTubeURL;
 @property (nonatomic, strong, readonly) NSURL *extractedURL;
-@property (nonatomic, unsafe_unretained) IBOutlet id <LBYouTubeExtractorDelegate> delegate;
+@property (nonatomic, weak) IBOutlet id <LBYouTubeExtractorDelegate> delegate;
+@property (nonatomic, strong) LBYouTubeExtractorCompletionBlock completionBlock;
+@property (nonatomic, strong) NSString* extractionExpression;
 
 -(id)initWithURL:(NSURL*)videoURL quality:(LBYouTubeVideoQuality)quality;
 -(id)initWithID:(NSString*)videoID quality:(LBYouTubeVideoQuality)quality;
 
 -(void)startExtracting;
 -(void)stopExtracting;
+
+-(void)extractVideoURLWithCompletionBlock:(LBYouTubeExtractorCompletionBlock)completionBlock;
 
 @end
 @protocol LBYouTubeExtractorDelegate <NSObject>
